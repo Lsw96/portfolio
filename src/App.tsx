6bed2c6c1e1c6bcc3eafa6c2c3311da64669c643
Leeseongwoo <tjddn1007@naver.com>
2024-09-26 21:59:43 +0900

    Design | 색상 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
 
+// components
 import Layout from '@components/Layout';
 
 const App: React.FC = () => {
 	return (
-		<>
+        <>
 			<div id="App">
 				<BrowserRouter>
 					<Layout>
