93503624778107c404a0840e4f0826f79a47494a
Leeseongwoo <tjddn1007@naver.com>
2024-09-27 16:52:52 +0900

    Feat | 이미지 컴포넌트

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,19 @@
 declare module '*.module.scss' {
 	const classes: { [key: string]: string };
-	export default classes; 
+	export default classes;
 }
 
 declare module '*.svg' {
 	const content: any;
 	export default content;
+}
+
+declare module '*.png' {
+	const value: string;
+	export default value;
+}
+
+declare module '*.jpg' {
+	const value: string;
+	export default value;
 }
